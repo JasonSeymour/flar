2aa01796d20da61f5712f4325b2aab2847be6b7f
Jason Seymour <jason.seymour@sargento.com>
2019-09-30 15:08:34 -0500

    system_archiver.pl:   - Changed flar filename to include a date stamp and a .flar suffix

--- system_archiver.sh
+++ system_archiver.sh
@@ -9,3 +9,12 @@ EXCLUDES=${HOST}.excludes
 
 [ ! -f ${ARCHIVE_BASE}/${HOST}/${EXCLUDES} ] && printf "/oracle\n/sapdb\n/sapmnt\n/usr/sap\n" > ${ARCHIVE_BASE}/${HOST}/${EXCLUDES}
 
+FSTYPE=$(df -n / | awk '{print $3}')
+
+case $FSTYPE in
+	ufs) printf "Root filesystem is ufs\n"
+	     eval "flarcreate -R / -X ${ARCHIVE_BASE}/${HOST}/${EXCLUDES} -L cpio -n ${HOST} ${ARCHIVE_BASE}/${HOST}/${HOST}.${DATESTAMP}.flar"
+	     printf "FLAR created\n"
+	     ;;
+esac
+
