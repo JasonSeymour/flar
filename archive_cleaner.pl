1874e4e573a28d3ed5d199000c7362022b8feb45
Jason Seymour <jason.seymour@sargento.com>
2019-11-04 15:51:08 -0600

    Replaced DateTime with Date::Calc All functions now implemented Still working on format of e-mail message

--- archive_cleaner.pl
+++ archive_cleaner.pl
@@ -2,9 +2,8 @@
 
 use strict;
 use warnings;
-use DateTime;
-use DateTime::Format::Strptime;
-use DateTime::Duration;
+use Date::Calc qw(Today Delta_Days);
+use Net::SMTP;
 
 my $root = '/flars';
 my %hosts;
@@ -13,7 +12,7 @@ die "Unable to access $root or is not a directory\n" unless -d $root;
 
 opendir(my $DIR, $root) or die "Unable to open $root for reading: $!\n";
 
-my $nowDate = DateTime->now;
+my ($nowYear,$nowMonth,$nowDay) = Today();
 
 while(my $fileObj = readdir $DIR)
 {
@@ -24,31 +23,25 @@ while(my $fileObj = readdir $DIR)
 	next if $fileObj eq '.' or $fileObj eq '..';	
 	# Skip the .snapshot subdirectory
 	next if $fileObj eq '.snapshot';
-	print("Processing host: $fileObj\n");
 	$hosts{$fileObj} = 0;
 	opendir(my $SUBDIR, $hostDir) or die "Unable to open the hostname based subdirectory $hostDir: $!\n";
 	my @flars = grep(/\.flar$/,readdir($SUBDIR));
 	foreach my $flar (@flars)
 	{
 		my ($name,$dateCode,$extension) = split(/\./,$flar);
-		my $strp = DateTime::Format::Strptime->new(pattern   => '%Y%m%d',);
-		my $snapDate = $strp->parse_datetime($dateCode);
-		my $delta = $nowDate->delta_md($snapDate);
-		my $days = $delta->days;
-		print("Datecode is $dateCode\n");
-		print("DateTime date is $snapDate->ymd\n");
-		print("Snapshot $flar is $days days old\n");
+		my ($snapYear,$snapMonth,$snapDay) = split(/\-/,$dateCode);
+		my $ddays = Delta_Days($snapYear,$snapMonth,$snapDay,$nowYear,$nowMonth,$nowDay);
 		# Set a flag if this snapshot is less than 32 days old so we can determine if a snapshot was missed
-		#if($days < 32)
-		#{
-		#	$hosts{$fileObj} = 1;
-		#} 
-		#elsif($days > 95)
-		#{
-		#	# When snapshot files reach this age we delete them
-		#	print("Deleting $flar\n");
-		#	#unlink($flar) or die "Unable to delete $flar because: $!\n";
-		#}
+		if($ddays < 32)
+		{
+			$hosts{$fileObj} = 1;
+		} 
+		elsif($ddays > 95)
+		{
+			# When snapshot files reach this age we delete them
+			my $fullPath = $hostDir . '/' . $flar;
+			unlink($fullPath) or die "Unable to delete $fullPath because: $!\n";
+		}
 
 	}
 	closedir($SUBDIR);
@@ -56,4 +49,26 @@ while(my $fileObj = readdir $DIR)
 closedir($DIR);
 
 # Walk the host hash and call out any hosts without a recent snapshot
+
+my $smtp = Net::SMTP->new('mailhost.sargento.com');
+$smtp->mail($ENV{USER});
+if ($smtp->to('jason.seymour@sargento.com'))
+{
+	$smtp->data();
+	$smtp->datasend("To: ITUnixAdmins\n");
+	$smtp->datasend("Subject: Solaris FLAR status\n");
+	$smtp->datasend("\n");
+	$smtp->datasend("Beginning of host report\n");
+	foreach my $host (keys(%hosts))
+	{
+		if($hosts{$host} == 0)
+		{
+			$smtp->datasend("Host $host is missing a recent flash archive\n\n");
+		}
+	}
+	$smtp->dataend();
+} else {
+	print "Error: ", $smtp->message();
+}
+$smtp->quit();
 
